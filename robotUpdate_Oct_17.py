3d99d8477fa6ddea426cdd5e535f6f5d4b8476f2
Manthena <GopiVarma.Manthena@geappliances.com>
2019-10-16 14:17:06 -0400

    added stove on motion, changed home to servo angles, added test file

--- robotUpdate_Oct_17.py
+++ robotUpdate_Oct_17.py
@@ -52,27 +52,27 @@ print(arm.last_used_joint_acc,arm.last_used_joint_speed)
 Pick handle and stir
 """
 
-
-arm.set_position(510,-225,100,-180,0,0,speed = 100, mvacc = 100, wait = True)
-
-
-myMainAct.verticalPick([510,-225,100,-180,0,0],{'z':-100},(300,0))
-myMainAct.approach(z = 200)
-myMainAct._achieveHorizontalGripperPos([500,0,300,-180,0,0])
-myMainAct.traverseWithPrevAttitude([500,0,450])
-myMainAct.approach(z = -120)
-myCkAct.stir(speed = 100,radius = 50,numTimes=5,wait = True)
-
-## get back to vertical position
-myMainAct._achieveVerticalGripperPos([500,0,400,-180,0,0])
-
-myMainAct.verticalPlace([510,-225,100,-180,0,0],{'z':-100},300)
-
-myMainAct.approach(z = 200)
-#arm.set_position(256,0,200,-180,0,0,speed = 100, mvacc = 100, wait = True)
-
-
-myMainAct.customGoHome()
+#
+# arm.set_position(510,-225,100,-180,0,0,speed = 100, mvacc = 100, wait = True)
+#
+#
+# myMainAct.verticalPick([510,-225,100,-180,0,0],{'z':-100},(300,0))
+# myMainAct.approach(z = 200)
+# myMainAct._achieveHorizontalGripperPos([500,0,300,-180,0,0])
+# myMainAct.traverseWithPrevAttitude([500,0,450])
+# myMainAct.approach(z = -120)
+# myCkAct.stir(speed = 100,radius = 50,numTimes=5,wait = True)
+#
+# ## get back to vertical position
+# myMainAct._achieveVerticalGripperPos([500,0,400,-180,0,0])
+#
+# myMainAct.verticalPlace([510,-225,100,-180,0,0],{'z':-100},300)
+#
+# myMainAct.approach(z = 200)
+# #arm.set_position(256,0,200,-180,0,0,speed = 100, mvacc = 100, wait = True)
+#
+#
+# myMainAct.customGoHome()
 
 
 """
@@ -81,29 +81,46 @@ pick glass and pour
 """
 
 
-myMainAct.horizontalPick([293,-225,50,-180,0,0],{'y':-100},
-                               (900,700),
-                               speed = None,mvacc=None,wait = None)
-myMainAct.approach(z = 200)
+# myMainAct.horizontalPick([293,-225,50,-180,0,0],{'y':-100},
+#                                (900,700),
+#                                speed = None,mvacc=None,wait = None)
+# myMainAct.approach(z = 200)
+#
+# myMainAct.traverseWithPrevAttitude([380,0,350])
+# myCkAct.pour(-100,speed=100,mvacc=20,wait=True)
+#
+# myMainAct.approach(x= -100)
+# myMainAct.horizontalPlace([293,-325,50,-180,0,0],{'y':-100},900)
+# myMainAct.customGoHome()
 
-myMainAct.traverseWithPrevAttitude([380,0,350])
-myCkAct.pour(-100,speed=100,mvacc=20,wait=True)
-
-myMainAct.approach(x= -100)
-myMainAct.horizontalPlace([293,-325,50,-180,0,0],{'y':-100},900)
+"""
+switch the stove on
+"""
 myMainAct.customGoHome()
 
+arm.set_position(499,-200,250,-180,0,0,speed = 100, mvacc = 100, wait = True)
+arm.set_position(yaw= -90, relative=True, wait =True,is_radian  = False)
+arm.set_position(pitch= 54, relative=True, wait =True,is_radian  = False)
+arm.set_gripper_position(200, wait=True)
+myMainAct.approach(z= -215)
+myMainAct.approach(y= 87)
 
+arm.set_gripper_position(110, wait=True)
 
+a = arm.get_servo_angle(is_radian=False)
+arm.set_servo_angle( angle=[a[1][0],a[1][1],a[1][2],a[1][3],a[1][4],a[1][5],a[1][6]+90],
+                     speed=None, mvacc=None, mvtime=None, is_radian=False,wait=True)
 
+arm.set_gripper_position(500, wait=True)
 
+arm.set_servo_angle( angle=[a[1][0],a[1][1],a[1][2],a[1][3],a[1][4],a[1][5],a[1][6]],
+                     speed=None, mvacc=None, mvtime=None, is_radian=False,wait=True)
 
+arm.set_gripper_position(500, wait=True)
 
+myMainAct.traverseWithPrevAttitude([499,-150,35])
 
+myMainAct.approach(z= 100)
 
-
-
-
-
-
+myMainAct.customGoHome()
 arm.disconnect()
